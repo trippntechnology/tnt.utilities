17fc4d730949d5898bd210884265e007f81ca314
Steve Tripp <steve@trippntechnology.com>
2024-01-20 11:39:49 -0700

    Update unit tests

---
+++ Tests/Resources/JsonTestClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Tests.Resources;
+
+[ExcludeFromCodeCoverage]
+internal class JsonTestClass
+{
+  public List<BaseClass> ListExample { get; set; } = new List<BaseClass>();
+  public int IntExample { get; set; } = 0;
+  public string StringExample { get; set; } = "";
+
+  public override bool Equals(object? obj)
+  {
+    var testObj = obj as JsonTestClass;
+    if (testObj == null) return false;
+    return IntExample == testObj.IntExample && StringExample == testObj.StringExample;
+  }
+}
